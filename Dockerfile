8be4eb4e87e4dcef95bab2c26639882e46ae306b
Daniel Mellado <dmellado@redhat.com>
2020-11-05 12:42:43 +0100

    Fix Dockerfile generation
    This PR fixes the Dockerfile generation and splits it in two, so they go
    ahead and use community/rhel8. It also refactors some stuff and adds
    tooling under the hack directory.
    
    Signed-off-by: Daniel Mellado <dmellado@redhat.com>

---
+++ Dockerfile
@@ -0,0 +1,9 @@
+FROM golang:1.15
+ADD . /usr/src/egress-router-cni
+RUN mkdir -p $GOPATH/src/github.com/openshift/egress-router-cni
+WORKDIR $GOPATH/src/github.com/openshift/egress-router-cni
+COPY . .
+RUN ./hack/build-go.sh
+
+FROM alpine:latest
+COPY --from=0 /go/src/github.com/openshift/egress-router-cni/bin/egress-router /usr/src/egress-router-cni/bin/egress-router
--- hack/build-go.sh
+++ hack/build-go.sh
@@ -1,19 +1,9 @@
 #!/usr/bin/env bash
-
 set -eu
-
-REPO=github.com/openshift/egress-router-cni
-CMD="egress-router"
-GLDFLAGS=${GLDFLAGS:-}
-GOFLAGS=${GOFLAGS:--mod=vendor}
-
+cmd=egress-router
 eval $(go env | grep -e "GOHOSTOS" -e "GOHOSTARCH")
-
-: "${GOOS:=${GOHOSTOS}}"
-: "${GOARCH:=${GOHOSTARCH}}"
-
-cd "$(git rev-parse --show-cdup)"
-
-eval $(go env)
-GO111MODULE=${GO111MODULE} CGO_ENABLED=0 GOOS=${GOOS} GOARCH=${GOARCH} go build ${GOFLAGS} -ldflags "${GLDFLAGS}" -o bin/${CMD} ${REPO}/cmd/${CMD}
-#CGO_ENABLED=0 GOOS=${GOOS} GOARCH=${GOARCH} go build ${GOFLAGS} -ldflags "${GLDFLAGS}" -o bin/${cmd} cmd/${cmd}.go
+GOOS=${GOOS:-${GOHOSTOS}}
+GOARCH=${GOACH:-${GOHOSTARCH}}
+GOFLAGS=${GOFLAGS:-}
+GLDFLAGS=${GLDFLAGS:-}
+CGO_ENABLED=0 GOOS=${GOOS} GOARCH=${GOARCH} go build ${GOFLAGS} -ldflags "${GLDFLAGS}" -o bin/${cmd} cmd/${cmd}/${cmd}.go
